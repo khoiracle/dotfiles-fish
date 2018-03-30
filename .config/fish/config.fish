2021278d1cfa4340da1e4a8964bc2b0f0b3f9353
khoiracle <khoiracle@gmail.com>
2018-03-30 11:54:55 +0700

    Update some tmux and fzf config for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,14 @@
 source ~/.config/fish/.fish_aliases
 source ~/.config/fish/.fish_variable
 
+# FZF Setup 
 set -U FZF_FIND_FILE_COMMAND 'pt --hidden -g ""'
 set -U FZF_COMPLETE 1
+
+# Tmux
+function fs -d "Switch Tmux session"
+  tmux list-sessions -F "#{session_name}" | fzf --reverse | xargs tmux switch-client -t
+end
 
 # Convenience PATH
 set -x WORKSPACE "$HOME/Workspace"
@@ -10,6 +16,7 @@ set -x WORKSPACE "$HOME/Workspace"
 # Golang
 set -x GOPATH "$WORKSPACE/go"
 set -x PATH $GOPATH/bin $PATH
+set -x PATH $HOME/.bin $PATH
 
 # Remap vim to nvim
 if type -q nvim > /dev/null 2>&1
