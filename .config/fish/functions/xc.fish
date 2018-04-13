b265d09029c96177a5a15da0e9c7ee10f603545b
khoiracle <khoiracle@gmail.com>
2018-04-13 11:31:35 +0700

    Update xc function

--- .config/fish/functions/xc.fish
+++ .config/fish/functions/xc.fish
@@ -1,4 +1,10 @@
 function xc
-  open (find . -maxdepth 1 -name '*.xcworkspace' -or -name '*.xcodeproj')
+  set -l xcode_proj (find . -maxdepth 1 -name '*.xcworkspace' -or -name '*.xcodeproj' | tail -n 1)
+  if [ $xcode_proj ]
+    echo $xcode_proj
+    open $xcode_proj
+  else
+    echo "No xcworkspace/xcodeproj file found in the current directory."
+  end
 end
 
