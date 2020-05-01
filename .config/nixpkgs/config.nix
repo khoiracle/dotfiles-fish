a056314c4c80b90d50069b81fb8e62c29447e4e3
khoi <khoiracle@gmail.com>
2020-05-01 18:50:34 +0700

    Use nix-darwin

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,27 +1,3 @@
 {
   allowUnfree = true;  
-  packageOverrides = pkgs: with pkgs; {
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
-        aria2
-        ffmpeg
-        fzf
-        git
-        gitAndTools.git-extras
-        gnupg
-        go
-        htop
-        neovim
-        nodejs
-        python3
-        ripgrep
-        ruby
-        starship
-        tldr
-        tmux
-        youtube-dl
-      ];
-    };
-  };
 }
