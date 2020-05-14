02fbf1475942c5a210a664fb4fb894f1e2f212b3
khoi <khoiracle@gmail.com>
2020-05-14 21:40:06 +0700

    Add hammer spoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,25 @@
+hs.hotkey.bind({"cmd", "ctrl"}, "h", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
