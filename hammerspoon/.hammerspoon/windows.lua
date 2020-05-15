e9fd950b20d88501331d87431b150af8d1cf3b67
khoi <khoiracle@gmail.com>
2020-05-15 15:19:03 +0700

    move windows across screens

--- hammerspoon/.hammerspoon/windows.lua
+++ hammerspoon/.hammerspoon/windows.lua
@@ -1,6 +1,6 @@
 hs.window.animationDuration = 0
 
-function moveScreen(x, y, w, h)
+function moveWindow(x, y, w, h)
   return function() 
     local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -15,8 +15,22 @@ function moveScreen(x, y, w, h)
   end
 end
 
-hs.hotkey.bind(hyper, "h", moveScreen(0, 0, 0.5, 1))
-hs.hotkey.bind(hyper, "j", moveScreen(0, 0.5, 1, 0.5))
-hs.hotkey.bind(hyper, "k", moveScreen(0, 0, 1, 0.5))
-hs.hotkey.bind(hyper, "l", moveScreen(0.5, 0, 0.5, 1))
-hs.hotkey.bind(hyper, "SPACE", moveScreen(0, 0, 1, 1))
+function moveScreen(direction)
+  return function() 
+    local win = hs.window.focusedWindow()
+    if direction == "left" then
+        win:moveOneScreenWest()
+    elseif direction == "right" then
+        win:moveOneScreenEast()
+    end
+  end
+end
+
+hs.hotkey.bind(hyper, "h", moveWindow(0, 0, 0.5, 1))
+hs.hotkey.bind(hyper, "j", moveWindow(0, 0.5, 1, 0.5))
+hs.hotkey.bind(hyper, "k", moveWindow(0, 0, 1, 0.5))
+hs.hotkey.bind(hyper, "l", moveWindow(0.5, 0, 0.5, 1))
+hs.hotkey.bind(hyper, "SPACE", moveWindow(0, 0, 1, 1))
+
+hs.hotkey.bind(hyper, "n", moveScreen("right"))
+hs.hotkey.bind(hyper, "p", moveScreen("left"))
