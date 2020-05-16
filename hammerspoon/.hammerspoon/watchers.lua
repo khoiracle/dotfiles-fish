049911400fb0a13406eefdd03bbba2b4e820e296
khoi <khoiracle@gmail.com>
2020-05-16 22:04:10 +0700

    Bring finder to front

--- hammerspoon/.hammerspoon/watchers.lua
+++ hammerspoon/.hammerspoon/watchers.lua
@@ -18,3 +18,13 @@ end
 
 hs.audiodevice.watcher.setCallback(muteWhenSwitchingAudioDevice)
 hs.audiodevice.watcher.start()
+
+function applicationWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) then
+        if (appName == "Finder") then
+            appObject:selectMenuItem({"Window", "Bring All to Front"})
+        end
+    end
+end
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
