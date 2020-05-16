91bcad87ea84b47a782cd1da79b9a8b06b661f43
khoi <khoiracle@gmail.com>
2020-05-16 13:45:23 +0700

    Use MiroWindowsManager

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,9 +5,16 @@ hs.window.animationDuration = 0
 
 -- Spoons
 hs.loadSpoon("ReloadConfiguration")
+hs.loadSpoon("MiroWindowsManager")
+
 spoon.ReloadConfiguration:start()
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "k"},
+  right = {hyper, "l"},
+  down = {hyper, "j"},
+  left = {hyper, "h"},
+  fullscreen = {hyper, "f"}
+})
 
-require "windows" -- Window management
+-- Custom modules
 require "watchers" -- Watchers for system event
-
-
