df56c518900a6b27f44a903e8cf85789a4b1a359
khoi <khoiracle@gmail.com>
2020-05-16 13:22:59 +0700

    Mute when switching audio device

--- hammerspoon/.hammerspoon/watchers.lua
+++ hammerspoon/.hammerspoon/watchers.lua
@@ -4,5 +4,17 @@ function muteOnWake(eventType)
     output:setMuted(true)
   end
 end
+
 caffeinateWatcher = hs.caffeinate.watcher.new(muteOnWake)
 caffeinateWatcher:start()
+
+function muteWhenSwitchingAudioDevice(eventType)
+  if eventType ~= "dOut" then
+    return
+  end
+  local output = hs.audiodevice.defaultOutputDevice()
+  output:setMuted(true)
+end
+
+hs.audiodevice.watcher.setCallback(muteWhenSwitchingAudioDevice)
+hs.audiodevice.watcher.start()
