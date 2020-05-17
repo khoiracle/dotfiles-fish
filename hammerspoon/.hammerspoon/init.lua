32e9e4e4ebf3b8c5c905fdda0669a81dea23cc27
khoi <khoiracle@gmail.com>
2020-05-17 12:14:24 +0700

    Simplify window manager code

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,27 +1,29 @@
 -- Custom modules
 require "watchers" -- Watchers for system event
 require "appswitch" -- Quick app switch
+require "windows" -- Windows functions
 
--- Configurations
+-- Global Configurations
 hs.window.animationDuration = 0
 hyper = {"ctrl", "alt"}
+shiftHyper = {"ctrl", "alt" , "shift"}
 
 -- Spoons
 hs.loadSpoon("ReloadConfiguration")
-hs.loadSpoon("MiroWindowsManager")
-
 spoon.ReloadConfiguration:start()
 
--- Window management
-spoon.MiroWindowsManager.fullScreenSizes = {1}
-spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "k"},
-  right = {hyper, "l"},
-  down = {hyper, "j"},
-  left = {hyper, "h"},
-  fullscreen = {hyper, "f"}
-})
+-- Window management bindings
+hs.hotkey.bind(hyper, "h", hs.fnutils.partial(moveWindow, 0, 0, 0.5, 1))
+hs.hotkey.bind(hyper, "j", hs.fnutils.partial(moveWindow, 0, 0.5, 1, 0.5))
+hs.hotkey.bind(hyper, "k", hs.fnutils.partial(moveWindow, 0, 0, 1, 0.5))
+hs.hotkey.bind(hyper, "l", hs.fnutils.partial(moveWindow, 0.5, 0, 0.5, 1))
+hs.hotkey.bind(hyper, "f", hs.fnutils.partial(moveWindow, 0, 0, 1, 1))
+hs.hotkey.bind(shiftHyper, "h", hs.fnutils.partial(moveScreen, "left"))
+hs.hotkey.bind(shiftHyper, "j", hs.fnutils.partial(moveScreen, "down"))
+hs.hotkey.bind(shiftHyper, "k", hs.fnutils.partial(moveScreen, "up"))
+hs.hotkey.bind(shiftHyper, "l", hs.fnutils.partial(moveScreen, "right"))
 
+-- App Switchers bindings
 hs.hotkey.bind(hyper, 't', hs.fnutils.partial(toggleApplication, "Alacritty"))
 hs.hotkey.bind(hyper, 'c', hs.fnutils.partial(toggleApplication, "Google Chrome"))
 hs.hotkey.bind(hyper, 'x', hs.fnutils.partial(toggleApplication, "Xcode"))
---
+++ hammerspoon/.hammerspoon/windows.lua
@@ -0,0 +1,25 @@
+function moveWindow(x, y, w, h)
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w * x + max.x
+  f.y = max.h * y
+  f.w = max.w * w
+  f.h = max.h * h
+  win:setFrame(f)
+end
+
+function moveScreen(direction)
+  local win = hs.window.focusedWindow()
+  if direction == "left" then
+    win:moveOneScreenWest()
+  elseif direction == "right" then
+    win:moveOneScreenEast()
+  elseif direction == "up" then
+    win:moveOneScreenNorth()
+  elseif direction == "down" then
+    win:moveOneScreenSouth()
+  end
+end
