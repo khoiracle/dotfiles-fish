de96fc9e9d3f8f734846f76d482b7dba7065a62e
khoi <khoiracle@gmail.com>
2020-05-18 16:28:29 +0700

    hyper-s shell switch

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -30,6 +30,6 @@ hs.hotkey.bind(shiftHyper, "k", hs.fnutils.partial(moveScreen, "up"))
 hs.hotkey.bind(shiftHyper, "l", hs.fnutils.partial(moveScreen, "right"))
 
 -- App Switchers bindings
-hs.hotkey.bind(hyper, 't', hs.fnutils.partial(toggleApplication, "Alacritty"))
+hs.hotkey.bind(hyper, 's', hs.fnutils.partial(toggleApplication, "Alacritty"))
 hs.hotkey.bind(hyper, 'c', hs.fnutils.partial(toggleApplication, "Google Chrome"))
 hs.hotkey.bind(hyper, 'x', hs.fnutils.partial(toggleApplication, "Xcode"))
