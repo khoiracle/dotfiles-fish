dbc42cf2e031ed457fc6500f4df0a1d9fe9d1331
khoi <git@khoi.io>
2021-03-08 21:02:20 +0700

    Add  shortcut to open ~/Downloads

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -41,3 +41,6 @@ hs.hotkey.bind(hyper, 'c', hs.fnutils.partial(toggleApplication, "Google Chrome"
 hs.hotkey.bind(hyper, 'x', hs.fnutils.partial(toggleApplication, "Xcode"))
 hs.hotkey.bind(hyper, 'f', hs.fnutils.partial(toggleApplication, "Finder"))
 hs.hotkey.bind(hyper, 't', hs.fnutils.partial(toggleApplication, "Telegram"))
+hs.hotkey.bind(hyper, 'd', function()
+  hs.execute("open ~/Downloads")
+end)
