248670e5569f6700885a3ad7b7cddcb731d7f47e
khoi <git@khoi.io>
2021-06-23 14:13:15 +0700

    update emacs config

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,17 +1,16 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
 (setq user-full-name "khoi"
       user-mail-address "khoiracle@gmail.com")
 
 (setq doom-font (font-spec :family "Jetbrains Mono" :size 14))
 (setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 ;; Reduce which-key delay
